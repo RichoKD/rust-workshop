3ade9c5d787f7aeb764bce8c0c9bc706789c8f91
sprtd <sagesdn@gmail.com>
2024-02-04 08:39:16 +0100

    chore: solve unused variable

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+
+// Fix the error below with least amount of modification to the code
+fn main() {
+    unused_var();
+}
+
+
+// UNUSED VARIABLES
+fn unused_var() {
+    let x: i32 = 5; // Uninitialized but used, ERROR !
+    let y: i32; // Uninitialized but also unused, only a Warning !
+
+    assert_eq!(x, 5);
+    println!("Success!");
+}
