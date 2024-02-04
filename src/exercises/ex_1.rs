1b2c78abaa68c9d2f9965d0f3a8cb02e7313f757
sprtd <sagesdn@gmail.com>
2024-02-04 09:22:42 +0100

    chore: update folder structure

--- src/exercises/ex_1.rs
+++ src/exercises/ex_1.rs
@@ -1,5 +1,6 @@
 
 // Ex 1 UNUSED VARIABLES
+// Fix the error below with least amount of modification to the code
 pub fn unused_var() {
     let x: i32 = 5; // Uninitialized but used, ERROR !
     let y: i32; // Uninitialized but also unused, only a Warning !
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-mod ex_1;
-mod ex_2;
-// Fix the error below with least amount of modification to the code
+mod exercises;
 fn main() {
-    ex_1::unused_var();
-    ex_2::mutate_variable();
+   exercises::ex_1::unused_var();
+   exercises::ex_2::mutate_variable();
 }
 
