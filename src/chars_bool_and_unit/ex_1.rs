4d60d740e3fd0a5cc028c69b70dbe255199f477b
sprtd <sagesdn@gmail.com>
2024-05-17 19:24:27 +0100

    chore(chars_bool_unit): solve ex_1

---
+++ src/chars_bool_and_unit/ex_1.rs
@@ -0,0 +1,15 @@
+// Make it work
+use std::mem::size_of_val;
+pub fn ex_1() {
+    let c1 = 'a';
+    println!("c1___{}", c1);
+    let result = size_of_val(&c1);
+    println!("result here___{}", result);
+    assert_eq!(size_of_val(&c1),4); 
+    
+    let c2 = '中';
+    let c2_result: usize = size_of_val(&c2);
+    println!("c2 result here___{}", c2_result);
+    assert_eq!(size_of_val(&c2),4); 
+    println!("Success!");
+} 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod exercises;
 mod integers;
+mod chars_bool_and_unit;
 fn main() {
     // exercises::ex_1::unused_var();
     // exercises::ex_2::mutate_variable();
@@ -9,10 +10,11 @@ fn main() {
     // exercises::ex_5::unused_var();
     // exercises::ex_6::destructure();
     // exercises::ex_7::destructure_assignment();
-    integers::ex_1::int_ex_1();
-    integers::ex_2::int_ex_2();
-    integers::ex_3::int_ex_3();
-    integers::ex_4::int_ex_4();
-    integers::ex_5::int_ex_5();
-    integers::ex_6::int_ex_6();
+    // integers::ex_1::int_ex_1();
+    // integers::ex_2::int_ex_2();
+    // integers::ex_3::int_ex_3();
+    // integers::ex_4::int_ex_4();
+    // integers::ex_5::int_ex_5();
+    // integers::ex_6::int_ex_6();
+    chars_bool_and_unit::ex_1::ex_1();
 }
