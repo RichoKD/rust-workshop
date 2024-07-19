57b8e9a1b11b86c07d3fa805ca7f740b9e38cacb
Abeeujah <abeeujah@gmail.com>
2024-07-19 22:23:33 +0100

    chore: ownership exercise 1-5

--- src/ownership/ex_1.rs
+++ src/ownership/ex_1.rs
@@ -1,12 +1,16 @@
 pub fn main() {
     // Use as many approaches as you can to make it work
     // let x: String = String::from("Hello world"); 
-    // let y: &String = &x; // approach 1
+    // let y: &String = &x; // approach 1 using references(Borrowing)
 
-    // let y = x.clone(); // approach 2
+    // let y = x.clone(); // approach 2 using clone method (making a replica of the data)
     // println!("{}, {}", x, y);
 
-    let x: &str = "Hello World"; // approach 3
+    let x: &str = "Hello World"; // approach 3 using string slices (Which are stored on the stack and can be copied.)
     let y = x;
     println!("{}, {}", x, y);
+
+    // let x = &String::from("Hello World");
+    // let y = x;
+    // println!("{}, {}", x, y);
 }
---
+++ src/ownership/ex_4.rs
@@ -0,0 +1,15 @@
+// Fix the error without removing any code
+pub fn main() {
+    let s = String::from("Hello World");
+
+    print_str(s.clone()); // Approach 1, clone the value (make a replica of the data) and pass it to the print_str() function
+    // print_str(&s); Approach 2, Reference the data stored in s (Borrowing) and pass the reference to the print_str() function
+
+    println!("Hello world from main()");
+    println!("{}", s);
+}
+
+fn print_str(s: String) {
+    println!("Hello world from print_str()");
+    println!("{}",s)
+}
